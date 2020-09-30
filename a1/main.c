cccfad0f5c8f4747dd259025498de2f3ad945209
keithradford <khr8771@gmail.com>
2020-09-29 19:21:42 -0700

    tried maing an array

--- a1/main.c
+++ a1/main.c
@@ -5,42 +5,71 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-#define CMD_BG = "bg"
-#define CMD_BGLIST = "bglist"
-#define CMD_BGKILL = "bgkill"
-#define CMD_BGSTOP = "bgstop"
-#define CMD_BGCONT = "bgstart"
-#define CMD_PSTAT = "pstat"
+#define CMD_BG "bg"
+#define CMD_BGLIST "bglist"
+#define CMD_BGKILL "bgkill"
+#define CMD_BGSTOP "bgstop"
+#define CMD_BGCONT "bgstart"
+#define CMD_PSTAT "pstat"
+#define ARRAY_SIZE 10
+#define ARG_SIZE 40
+
+void bg_entry(char **argv);
+void check_zombieProcess(void);
 
 int main(){
 
 	while(1){	
 		char *cmd = readline("PMan: > ");
 		
-		/* parse the input cmd (e.g., with strtok)
-		 */
 		const char *split = " ";
    		char *cmd_type;
    		char *pid;
+   		char tmp[ARRAY_SIZE][ARG_SIZE];
+   		char *arg;
    		
+   		// parse command and arguments from readline
     	cmd_type = strtok(cmd, split);
+    	// pid = strtok(NULL, split);
+    	arg = strtok(NULL, split);
 
+    	// put arguments into a temp array of strings
+    	int i = 0;
+		while(arg != NULL){
+			strcpy(tmp[i], arg);
+			arg = strtok(NULL, split);
+			i++;
+		}
 
+		// put arguments into appropriately sized array
+		char argv[i][ARG_SIZE];
+	    printf("first: %s\nsecond: %s\n", cmd_type, pid);
+		size_t n = sizeof(argv)/sizeof(argv[0]);
+		for(int i = 0; i < n; i++){
+			strcpy(argv[i], tmp[i]);
+			if(argv[i]){
+				printf("%s\n", argv[i]);
+			}
+		}
+
+		// call appropriate function based on command
 		if (cmd_type == CMD_BG){
 			bg_entry(argv);
 		}
-		else if(cmd_type == CMD_BGLIST){
-			bglist_entry();
-		}
-		else if(cmd_type == CMD_BGKILL || cmd_type == CMD_BGSTOP || cmd_type == CMD_BGCONT){
-			bgsig_entry(pid, cmd_type);
-		}
-		else if(cmd_type == CMD_PSTAT){
-			pstat_entry(pid);
-		}
-		else {
-			usage_pman();
-		}
+		// else if(cmd_type == CMD_BGLIST){
+		// 	bglist_entry();
+		// }
+		// else if(cmd_type == CMD_BGKILL || cmd_type == CMD_BGSTOP || cmd_type == CMD_BGCONT){
+		// 	pid = argv[0];
+		// 	bgsig_entry(pid, cmd_type);
+		// }
+		// else if(cmd_type == CMD_PSTAT){
+		// 	pid = argv[0];
+		// 	pstat_entry(pid);
+		// }
+		// else {
+		// 	usage_pman();
+		// }
 		check_zombieProcess();
 	}
 	return 0;
@@ -69,22 +98,22 @@ void check_zombieProcess(void){
 	int status;
 	int retVal = 0;
 	
-	while(1) {
-		usleep(1000);
-		if(headPnode == NULL){
-			return ;
-		}
-		retVal = waitpid(-1, &status, WNOHANG);
-		if(retVal > 0) {
-			//remove the background process from your data structure
-		}
-		else if(retVal == 0){
-			break;
-		}
-		else{
-			perror("waitpid failed");
-			exit(EXIT_FAILURE);
-		}
-	}
+	// while(1) {
+	// 	usleep(1000);
+	// 	if(headPnode == NULL){
+	// 		return ;
+	// 	}
+	// 	retVal = waitpid(-1, &status, WNOHANG);
+	// 	if(retVal > 0) {
+	// 		//remove the background process from your data structure
+	// 	}
+	// 	else if(retVal == 0){
+	// 		break;
+	// 	}
+	// 	else{
+	// 		perror("waitpid failed");
+	// 		exit(EXIT_FAILURE);
+	// 	}
+	// }
 	return ;
 }
--- a1/test.c
+++ a1/test.c
@@ -5,6 +5,16 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+// void expand_array(char *arr[]){
+// 	size_t n = sizeof(arr)/sizeof(arr[0]);
+// 	char tmp[n*2][40];
+// 	for(int i = 0; i < n; i++){
+// 		strcpy(tmp[i], arr[i]);
+// 	}
+// 	tmp[n*2] = '\0';
+// 	*arr = tmp;
+// } 
+
 
 int main(){
 
@@ -18,15 +28,36 @@ int main(){
     const char *split = " ";
     char *token;
     char *pid;
-    char *argv;
+    char **argv = malloc(10 * sizeof(char*));
+	char *arg;
 
     token = strtok(input, split);
-    pid = strtok(NULL, split);
-    argv = strtok(NULL, split);
+    // pid = strtok(NULL, split);
+    arg = strtok(NULL, split);
 
-    printf("first: %s\nsecond: %s\nthird: %s\n", token, pid, argv);
+    int i = 0;
+	while(arg != NULL){
+		// if(i == 10){
+		// 	expand_array(argv);
+		// }
+		argv[i] = malloc(strlen(arg) * sizeof(char));
+		strcpy(argv[i], arg);
+		free(argv[i]);
+		arg = strtok(NULL, split);
+		i++;
+	}
+	free(argv);
+
+    printf("first: %s\nsecond: %s\n", token, pid);
+
+	size_t n = sizeof(argv)/sizeof(argv[0]);
+
+	for(int i = 0; i < n; i++){
+		if(argv[i]){
+			printf("%s\n", argv[i]);
+		}
+	}
 
 
     return 1;
 }
-
