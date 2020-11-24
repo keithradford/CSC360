471de8636d3a6d9e5a1e146bfc053a19e047296a
keithradford <khr8771@gmail.com>
2020-11-24 13:19:48 -0800

    Finish part 3

--- a3/diskget.c
+++ a3/diskget.c
@@ -12,27 +12,32 @@
 
 #define ATTRIBUTE_OFFSET 11
 #define DATA_OFFSET 16896
-#define DIRECTORY_SIZE 3
+#define DIRECTORY_SIZE 32
 #define EXTENSION_SIZE 3
+#define FILE_SIZE_OFFSET 28
 #define FIRST_LOGICAL_CLUSTER_OFFSET 26
 #define NAME_SIZE 9
 #define ROOT_DIRECTORY 9728
 
 const char *getFileName(char *p, int start);
+void writeToFile(char *p, int start, const char *file);
 
 int main(int argc, char *argv[]){
 	// char *file_name = malloc( (NAME_SIZE + EXTENSION_SIZE + 1) * (sizeof(char)) );
 
 	int fd;
 	struct stat sb;
-	char *disk = malloc ( strlen(argv[1]) * sizeof(char) );
 
 	if(argc != 3){
 		printf("Error: diskget usage must be \n./diskget <DISK> <FILE>\n");
 		exit(1);
 	}
-	
+
+
+	char *disk = malloc ( strlen(argv[1]) * sizeof(char) );
+	char *file = malloc ( strlen(argv[2]) * sizeof(char) );
 	strcpy(disk, argv[1]);	
+	strcpy(file, argv[2]);
 
 	fd = open(disk, O_RDWR);
 	fstat(fd, &sb);
@@ -43,6 +48,7 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
+	int found = 0;
 	for(int i = ROOT_DIRECTORY; i < DATA_OFFSET; i += DIRECTORY_SIZE){
 		int high = p[i + FIRST_LOGICAL_CLUSTER_OFFSET + 1] << 8;
 		int low = p[i + FIRST_LOGICAL_CLUSTER_OFFSET];
@@ -58,11 +64,18 @@ int main(int argc, char *argv[]){
 		){
 			continue;
 		}
-		// printf("%d\n", i);
 		const char *file_name = getFileName(p, i);
-		// strcpy(file_name, getFileName(p, i));
-		printf("%s\n", file_name);
-		// break;
+		if(strcmp(file_name, file) == 0){
+			printf("Matched: %s and %s\n", file_name, file);
+			writeToFile(p, i, file_name);
+			found = 1;
+			break;
+		}
+	}
+
+	if(!found){
+		printf("File not found.\n");
+		exit(1);
 	}
 
 	munmap(p, sb.st_size);
@@ -84,7 +97,7 @@ const char *getFileName(char *p, int start){
 		// printf("yo %c\n", p[start + j]);
 	}
 	name[j] = '\0';
-	
+
 	int i = 0;
 	for(int k = 8; k < 11; k++){
 		// printf("%c\n", p[start + k]);
@@ -111,4 +124,29 @@ const char *getFileName(char *p, int start){
 	// printf("%s\n", file_name);
 
 	return file_name;
+}
+
+void writeToFile(char *p, int start, const char *file){
+	int high = p[start + FIRST_LOGICAL_CLUSTER_OFFSET + 1] << 8;
+	int low = p[start + FIRST_LOGICAL_CLUSTER_OFFSET];
+	int first_logical_cluster = high + low;
+
+	int fourth = (p[start + FILE_SIZE_OFFSET + 3] & 0xFF) << 24;
+	int third = (p[start + FILE_SIZE_OFFSET + 2] & 0xFF) << 16;
+	int second = (p[start + FILE_SIZE_OFFSET + 1] & 0xFF) << 8;
+	int first = p[start + FILE_SIZE_OFFSET] & 0xFF;
+	int file_size = first + third + second + first;
+
+	// printf("%x %x %x %x\n", fourth, third, second, first);
+
+	// printf("First Logical Cluster: %x\nFile Size: %d\n", first_logical_cluster, file_size);
+	FILE *fp;
+	fp = fopen(file, "w");
+	int sector = (33 + first_logical_cluster - 2) * 512;
+	for(int i = 0; i < file_size; i++){
+		if((p[sector + i] & 0xFF) == 0x00)
+			break;
+		fputc(p[sector + i], fp);
+	}
+	fclose(fp);
 }
